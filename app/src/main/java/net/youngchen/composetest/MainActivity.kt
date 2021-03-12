78a2da12e4a647b1a6302aacfc28dec6807f1e6e
Yang Chen <yang.chen@rea-group.com>
2021-03-12 19:49:46 +0800

    finish Jetpack Compose basics 4: Declarative UI

--- app/src/main/java/net/youngchen/composetest/MainActivity.kt
+++ app/src/main/java/net/youngchen/composetest/MainActivity.kt
@@ -3,36 +3,55 @@ package net.youngchen.composetest
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.youngchen.composetest.ui.theme.ComposeTestTheme
+import java.net.ContentHandler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MyApp { MyScreenContent() }
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeTestTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    Column {
+        for (name in names) {
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeTestTheme {
-        Greeting("Android")
-    }
+    MyApp { MyScreenContent() }
 }
