a6730a90b9ca202fd8afcac7329f36c53b9e0ac6
Chen Yang <chennyoung@126.com>
2021-03-17 23:21:59 +0800

    finish 5 State in Compose

--- app/src/main/java/net/youngchen/composetest/MainActivity.kt
+++ app/src/main/java/net/youngchen/composetest/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,11 +41,25 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    val counterState = remember { mutableStateOf(0) }
+
     Column {
         for (name in names) {
             Greeting(name = name)
             Divider(color = Color.Black)
         }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter(count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            })
+    }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count + 1) }) {
+        Text(text = "I've been clicked $count times")
     }
 }
 
